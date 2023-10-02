ab28297ff2360fa1fbb5f2f62e7fcfc9f4b5e653
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-02 20:20:09 +0100

    fix: modify

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import './Modal.css';
 
 const Modal = ({ isOpen, onClose, children }) => {
   const [showModal, setShowModal] = useState(isOpen);
 
+  const handleClose = useCallback(() => {
+    setShowModal(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [onClose]);
+
+  useEffect(() => {
+    setShowModal(isOpen)
+  }, [isOpen]);
+
+  if (!isOpen) {
+    return null;
+  }
+
   return (
     <div className='modal'>
-      <div className='modal-wrapper active'>
+      <div className={showModal ? 'modal-wrapper active' : 'modal-wrapper'}>
         <div className='modal-content'>
           {children}
         </div>
