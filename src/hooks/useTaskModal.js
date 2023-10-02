38c8b8b0bb2396cb1b0dec4d57bfc7019df53089
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-03 00:55:03 +0100

    fix: change file name

---
+++ src/hooks/useTaskModal.js
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+
+const store = (set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+});
+
+export const useModal = create(store);
