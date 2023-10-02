8aa0fdee187aefa6087233db136e25ad5be3e3b0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-02 15:30:37 +0100

    fix: add markup

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,9 +1,17 @@
 import './Task.css';
 
+const STATUS = 'PLANNED';
+
 const Task = ({ title }) => {
   return (
-    <div className='task'>{title}</div>
-  )
-}
+    <div className='task'>
+      <div>{title}</div>
+      <div>
+        <div></div>
+        <div className='status'>{STATUS}</div>
+      </div>
+    </div>
+  );
+};
 
-export default Task
+export default Task;
