8a9fa77269c0eb970fa1d41cd8ac0f37f7f69af5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-03 00:54:57 +0100

    fix: change file name

--- src/hooks/useModal.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const store = (set) => ({
-  isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
-});
-
-export const useModal = create(store);
