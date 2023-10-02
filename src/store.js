2d37c1192dd9420066bb4e83ba703b8f5df5d478
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-02 18:57:03 +0100

    fix: modify

--- src/store.js
+++ src/store.js
@@ -4,6 +4,9 @@ const store = (set) => ({
   tasks: [
     { title: 'Test Task', status: 'PLANNED' },
   ],
-})
+  addTask: (title, status) => set((state) => ({
+    tasks: [...state.tasks, { title, status }]
+  })),
+});
 
 export const useStore = create(store);
