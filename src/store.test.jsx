33c4f6fe9c218ab57addb04fdb38d9b0b74b64ed
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-03 22:59:20 +0100

    fix: mock zustand

--- src/store.test.jsx
+++ src/store.test.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
+import { render } from '@testing-library/react';
 import { expect, test, vi } from 'vitest';
 
 import { useTask } from './store';
-import { render } from '@testing-library/react';
+
+vi.mock('zustand');
 
 function TestComponent({ selector, effect }) {
   const items = useTask(selector);
@@ -30,6 +32,32 @@ test('should add an item to the store and rerun effect', () => {
         title: 'a',
         status: 'b',
       });
+    }
+  });
+  render(<TestComponent selector={selector} effect={effect} />);
+  expect(effect).toHaveBeenCalledTimes(2);
+  expect(effect).toHaveBeenCalledWith(expect.objectContaining({ tasks: [{ id: 1, title: 'a', status: 'b' }] }));
+});
+
+test('should add an item to the store and rerun effect', () => {
+  const selector = (store) => ({
+    tasks: store.tasks,
+    addTask: store.addTask,
+    deleteTask: store.deleteTask,
+  });
+  let createdTask = false;
+
+  const effect = vi.fn().mockImplementation((items) => {
+    if (!createdTask) {
+      items.addTask({
+        id: 1,
+        title: 'a',
+        status: 'b',
+      });
+
+      createdTask = true;
+    } else if (items.tasks.length === 1) {
+      items.deleteTask(1);
     }
   });
   render(<TestComponent selector={selector} effect={effect} />);
