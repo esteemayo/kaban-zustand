1d4e1a25603f4c43aebc0ad638b3d60475921cff
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-03 18:13:35 +0100

    fix: write test

--- src/store.test.jsx
+++ src/store.test.jsx
@@ -20,3 +20,19 @@ test('should return default value at the start', () => {
   render(<TestComponent selector={selector} effect={effect} />);
   expect(effect).toHaveBeenCalledWith([]);
 });
+
+test('should add an item to the store and rerun effect', () => {
+  const selector = (store) => ({ tasks: store.tasks, addTask: store.addTask });
+  const effect = vi.fn().mockImplementation((items) => {
+    if (items.tasks.length === 0) {
+      items.addTask({
+        id: 1,
+        title: 'a',
+        status: 'b',
+      });
+    }
+  });
+  render(<TestComponent selector={selector} effect={effect} />);
+  expect(effect).toHaveBeenCalledTimes(2);
+  expect(effect).toHaveBeenCalledWith(expect.objectContaining({ tasks: [{ id: 1, title: 'a', status: 'b' }] }));
+});
