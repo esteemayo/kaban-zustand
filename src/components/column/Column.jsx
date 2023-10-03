1e872731a69858a713e81ea2bfb043e6bc9f111f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-03 01:18:35 +0100

    fix: refactor

--- src/components/column/Column.jsx
+++ src/components/column/Column.jsx
@@ -3,20 +3,22 @@ import { v4 as uuidV4 } from 'uuid';
 import { useCallback, useState } from 'react';
 
 import Task from '../task/Task';
+import { useTaskModal } from '../../hooks/useTaskModal';
 import { useTask } from '../../hooks/useTask';
+
+import Modal from '../modal/Modal';
 
 import './Column.css';
 
 const Column = ({ status }) => {
+  const { isOpen, onOpen, onClose } = useTaskModal();
   const tasks = useTask((state) =>
     state.tasks.filter((item) => item.status === status),
     shallow
   );
-
   const addTask = useTask((state) => state.addTask);
 
   const [text, setText] = useState('');
-  const [open, setOpen] = useState(false);
 
   const handleClick = useCallback(() => {
     const newTask = {
@@ -27,34 +29,36 @@ const Column = ({ status }) => {
 
     addTask(newTask);
     setText('');
-    setOpen(false);
-  }, [addTask, status, text]);
+    onClose();
+  }, [addTask, onClose, status, text]);
+
+  const bodyContent = (
+    <>
+      <input
+        type='text'
+        value={text}
+        placeholder='Title'
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={handleClick}>Submit</button>
+    </>
+  );
 
   return (
     <div className='column'>
       <div className='title-wrapper'>
         <p>{status}</p>
-        <button onClick={() => setOpen(true)}>Add</button>
+        <button onClick={() => onOpen()}>Add</button>
       </div>
       {tasks.map((item) => {
         const { id, title } = item
         return <Task key={id} title={title} />
       })}
-      {open && (
-        <div className='modal'>
-          <div className={open ? 'modal-wrapper active' : 'modal-wrapper'}>
-            <div className='modal-content'>
-              <input
-                type='text'
-                value={text}
-                placeholder='Title'
-                onChange={(e) => setText(e.target.value)}
-              />
-              <button onClick={handleClick}>Submit</button>
-            </div>
-          </div>
-        </div>
-      )}
+      <Modal
+        isOpen={isOpen}
+        body={bodyContent}
+        onClose={onClose}
+      />
     </div>
   );
 };
